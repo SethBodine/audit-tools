35a376348e3bc783a2484eca23ccc525cb570cee
Seth Bodine <nope>
2024-05-28 10:38:56 +1200

    script podman start-up tweaks

--- pm-init.sh
+++ pm-init.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-echo Launching Container...
+echo Clean-up of Container
 
 podman stop audit-tools
 podman rm audit-tools
+
+echo Launching Container...
 podman pull ghcr.io/sethbodine/audit-tools && \
 podman image prune -f && \
 container_id=$(podman run -it -p 9194:9194 -p 9033:9033 -p 11666:11666 --rm --detach --name audit-tools ghcr.io/sethbodine/audit-tools /sbin/updatetools) && \
-podman exec -it --user container ${container_id} /bin/bash 
+podman exec -it --user container ${container_id} /bin/bash && \
 podman stop ${container_id}
 
 echo Container Cleaned Up...
+
