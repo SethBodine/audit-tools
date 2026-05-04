d2ebb9409a2a49004d90fb6d1317690f95f54e72
SethBodine <2039309+SethBodine@users.noreply.github.com>
2026-05-05 10:47:39 +1200

    fixing typo for cazadora

--- cazadora.sh
+++ cazadora.sh
@@ -11,6 +11,6 @@ if [ -n "${VIRTUAL_ENV+x}" ]; then deactivate; fi
 cd ${SCRIPT_PATH}  || exit #handle for cd failure
 . venv/bin/activate
 pip install --upgrade pip
-pip install -r requirments.txt
+pip install -r requirements.txt
 
 printf "\n\n%s Environment Ready... execute \"deactivate\" to exit the environment\n\n" "${APP_NAME}"
